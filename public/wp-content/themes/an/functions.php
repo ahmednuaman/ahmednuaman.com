1303cd0991dc53c801204b5284846f24f9cf08c9
Ahmed Nuaman, Freelance Designer and Developer <ahmed@ahmednuaman.com>
2011-06-21 00:37:17 +0100

    enabled thumbnails for clients and projects

--- public/wp-content/themes/an/functions.php
+++ public/wp-content/themes/an/functions.php
@@ -1,41 +1,37 @@
 <?php
 function an_setup()
 {
-	add_theme_support( 'post-thumbnails', array( 'post' ) );
+	add_theme_support( 'post-formats' );
+	add_theme_support( 'post-thumbnails' );
 	
 	add_image_size( 'an_hero', 900, 450 );
 	add_image_size( 'an_hero_mobile', 300, 150 );
 	add_image_size( 'an_thumbnail', 225, 150 );
-	
-	register_post_type( 'an_project', array(
-			'labels'		=> array(
-				'name'			=> __( 'Projects' ),
-				'singular_name'	=> __( 'Project' )
-			),
-			'supports'		=> array(
-				'title',
-				'thumbnail',
-				'custom-fields'
-			),
-			'show_ui'		=> true,
-			'show_in_menu'	=> true
-		)
-	);
-	
-	register_post_type( 'an_client', array(
-			'labels'		=> array(
-				'name'			=> __( 'Clients' ),
-				'singular_name'	=> __( 'Client' )
-			),
-			'supports'		=> array(
-				'title',
-				'thumbnail',
-				'custom-fields'
-			),
-			'show_ui'		=> true,
-			'show_in_menu'	=> true
-		)
-	);
+}
+
+function an_init()
+{
+	$a	= array( 'client', 'project' );
+	foreach ( $a as $n )
+	{
+		register_post_type( 'an_' . $n, array(
+				'labels'		=> array(
+					'name'			=> __( ucwords( $n ) . 's' ),
+					'singular_name'	=> __( ucwords( $n ) )
+				),
+				'supports'		=> array(
+					'title',
+					'thumbnail',
+					'custom-fields',
+					'editor',
+					'revisions'
+				),
+				'show_ui'		=> true,
+				'show_in_menu'	=> true
+			)
+		);
+	}
 }
 
 add_action( 'after_setup_theme', 'an_setup' );
+add_action( 'init', 'an_init' );
--- public/wp-content/themes/an/index.php
+++ public/wp-content/themes/an/index.php
@@ -20,26 +20,63 @@ $js		= $assets . 'js/';
 		<title><? wp_title( ' ~ ' ); ?></title>
 	</head>
 	<body <?php body_class(); ?>>
-		<?php
-		if ( is_front_page() )
-		{
-			?>
-			front page
-			<?php
-		}
-		else if ( is_home() )
-		{
-			?>
-			blog page
-			<?php
-		}
-		else if ( is_single() )
-		{
-			?>
-			blog post
-			<?php
-		}
-		?>
+		<div id="container">
+			<header>
+				<h1><?php bloginfo( 'name' ); ?></h1>
+				<?php if ( is_single() ): ?>
+					<?php while ( have_posts() ): the_post(); ?>
+						<?php $u	= wp_get_attachment_image_src( get_post_thumbnail_id(), 'an_hero' ); ?>
+						<div id="post_thumb" style="background-image: url(<?php echo $u[ 0 ]; ?>)"></div>
+						<?php $u	= wp_get_attachment_image_src( get_post_thumbnail_id(), 'an_hero_mobile' ); ?>
+						<div id="post_thumb_mobile" class="mobile" style="background-image: url(<?php echo $u[ 0 ]; ?>)"></div>
+					<?php endwhile; ?>
+					<?php rewind_posts(); ?>
+				<?php else: ?>
+					<div id="carousel">
+						<ul>
+							<?php get_posts( array( 'post_type' => is_front_page() ? 'an_project' : 'post' ) ); ?>
+							<?php while ( have_posts() ): the_post(); ?>
+								<?php $u	= wp_get_attachment_image_src( get_post_thumbnail_id(), 'an_hero' ); ?>
+								<li id="tile_<?php echo $post->ID; ?>" class="large" style="background-image: url(<?php echo $u[ 0 ]; ?>)">
+									<h3><?php the_title(); ?></h3>
+								</li>
+								<?php $u	= wp_get_attachment_image_src( get_post_thumbnail_id(), 'an_hero_mobile' ); ?>
+								<li id="tile_mobile_<?php echo $post->ID; ?>" class="mobile" style="background-image: url(<?php echo $u[ 0 ]; ?>)">
+									<h3><?php the_title(); ?></h3>
+								</li>
+							<?php endwhile; ?>
+							<?php rewind_posts(); ?>
+						</ul>
+					</div>
+				<?php endif; ?>
+			</header>
+			<div id="main" role="main">
+				<?php
+				if ( is_front_page() )
+				{
+					?>
+					front page
+					<?php
+				}
+				else if ( is_home() )
+				{
+					?>
+					blog page
+					<?php
+				}
+				else if ( is_single() )
+				{
+					?>
+					blog post
+					<?php
+				}
+				?>
+			</div>
+			<footer>
+				<p>&copy; Ahmed Nuaman (that's me) unless it's client work then the copyright sits with its respective owners. As for the blog, that's licensed under a <a href="http://creativecommons.org/licenses/by-sa/2.0/uk/">Creative Commons Attribution-Share Alike 2.0 UK: England &amp; Wales License</a>, unless I (Ahmed Nuaman) state otherwise on the post.</p>
+				<p>Built using <a href="http://wordpress.org">WordPress</a> because it's awesome. <a href="http://firestartermedia.com/?utm_referrer=ahmednuaman">Looking for web design and development? Try FireStarter Media Limited</a>.</p>
+			</footer>
+		</div>
 		<?php
 		$scripts		= array(
 			'jquery-core',
