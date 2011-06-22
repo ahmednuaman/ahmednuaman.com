9df7302159c24131b016f1be53e94afaa171aa10
Ahmed Nuaman, Freelance Designer and Developer <ahmed@ahmednuaman.com>
2011-06-22 22:05:05 +0100

    sorted out blog page

--- public/wp-content/themes/an/index.php
+++ public/wp-content/themes/an/index.php
@@ -105,14 +105,14 @@ $js			= $assets . 'js/';
 									'after' => '</p>', 
 									'next_or_number' => 'number' 
 								)); ?>
-								<div class="postmetadata alt">
+								<div class="alt">
 									<h3>Share the love:</h3>
 									<div class="noborder">
 										<iframe allowtransparency="true" frameborder="0" scrolling="no" src="http://platform.twitter.com/widgets/tweet_button.html?url=<?php the_permalink(); ?>&amp;via=ahmednuaman&amp;text=<? the_title(); ?>" style="width:130px; height:40px;"></iframe>
 										<iframe src="http://www.facebook.com/plugins/like.php?href=<?php the_permalink(); ?>&amp;layout=button_count&amp;show_faces=false&amp;width=100&amp;action=like&amp;font=arial&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:40px;" allowTransparency="true"></iframe>
 									</div>
 									<h3>Related posts:</h3>
-									<menu>
+									<ul>
 										<?php related_posts_by_category( array(
 											'orderby' => 'post_date',
 											'order' => 'DESC',
@@ -123,8 +123,11 @@ $js			= $assets . 'js/';
 											'type' => 'post',
 											'message' => 'no matches'
 										)); ?>
-									</menu>
-									<?php the_tags( 'Tags: ', ', ', ' &mdash; ' ); ?> Posted in: <?php the_category(', ') ?> on <?php the_time( 'l, F jS, Y' ); ?>.
+									</ul>
+									<div class="postmetadata">
+										<?php the_tags( 'Tags: ', ', ', ' &mdash; ' ); ?> Posted in: <?php the_category(', ') ?> on <?php the_time( 'F jS, Y' ); ?>.
+										<span></span>
+									</div>
 								</div>
 								<nav class="navigation">
 									<div class="right"><?php previous_posts_link( 'Newer Entries &raquo;' ); ?></div>
@@ -218,23 +221,22 @@ $js			= $assets . 'js/';
 						<?php dynamic_sidebar( 'blog_right' ); ?>
 					</section>
 					<section class="col colleft">
-						<div class="list">
-							<?php while ( have_posts() ): the_post(); ?>
-								<article>
-									<h3 id="post-<?php the_ID(); ?>">
-										<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
-											<?php the_title(); ?>
-										</a>
-									</h3>
-									<div class="entry">
-										<?php the_content( 'Read more &raquo;' ); ?>
-									</div>
-									<p class="postmetadata">
-										<?php the_tags( 'Tags: ', ', ', ' &mdash; ' ); ?> Posted in: <?php the_category(', ') ?> on <?php the_time( 'l, F jS, Y' ); ?>.
-									</p>
-								</article>
-							<?php endwhile; ?>
-						</div>
+						<?php while ( have_posts() ): the_post(); ?>
+							<article>
+								<h3 id="post-<?php the_ID(); ?>">
+									<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
+										<?php the_title(); ?>
+									</a>
+								</h3>
+								<div class="entry">
+									<?php the_content( 'Read more &raquo;' ); ?>
+								</div>
+								<div class="postmetadata">
+									<?php the_tags( 'Tags: ', ', ', ' &mdash; ' ); ?> Posted in: <?php the_category(', ') ?> on <?php the_time( 'F jS, Y' ); ?>.
+									<span></span>
+								</div>
+							</article>
+						<?php endwhile; ?>
 						<nav class="navigation">
 							<div class="right"><?php previous_posts_link( 'Newer Entries &raquo;' ); ?></div>
 							<div class="left"><?php next_posts_link( '&laquo; Older Entries' ); ?></div>
