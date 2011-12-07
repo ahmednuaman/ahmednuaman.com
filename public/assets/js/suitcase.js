dac7ad07570fdf1426f32557ce220b95ff1224ad
Ahmed Nuaman <ahmed@ahmednuaman.com>
2011-12-07 16:36:55 +0000

    sorted out scrolling ease

--- public/assets/js/suitcase.js
+++ public/assets/js/suitcase.js
@@ -14,6 +14,7 @@ var S	= {
 		S.loadTweets();
 		S.drawGradients();
 		S.addLettering();
+		S.sortScrolling();
 		
 		S.menuY	= $( '#menubar' ).offset().top;
 		
@@ -198,8 +199,33 @@ var S	= {
 		{
 			m.removeClass( c );
 		}
+	},
+	
+	sortScrolling												: function()
+	{
+		$( 'a[href^="#"]' ).click( function(e)
+		{
+			var y	= $( e.currentTarget.hash ).offset().top - $( '#menubar' ).outerHeight();
+			
+			$( 'html, body' ).animate({
+				scrollTop	: y
+			}, 2000, 'easeInOutExpo' );
+			
+			return false;
+		});
 	}
 };
+
+$.extend($.easing,
+{
+	def: 'easeOutQuad',
+	easeInOutExpo: function (x, t, b, c, d) {
+		if (t==0) return b;
+		if (t==d) return b+c;
+		if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+		return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+	}
+});
 
 // var _gaq=[['_setAccount','UA-352545-12'],['_trackPageview'],['_trackPageLoadTime']];
 // (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
