d4fdd5c0beb877417ff33a1ba64e3b541a6be2c2
Ahmed Nuaman <ahmed@ahmednuaman.com>
2011-12-08 15:53:31 +0000

    added file caching

--- private/wordpress/functions.php
+++ private/wordpress/functions.php
@@ -1,5 +1,62 @@
 <?php
 
+$ahmed_cache_folder	= dirname( dirname( dirname( __FILE__ ) ) ) . '/cache/';
+$ahmed_cache_prefix	= 'ahmed_';
+
+function ahmed_check_cache()
+{
+	global $ahmed_cache_folder;
+	global $ahmed_cache_prefix;
+	
+	$f	= $ahmed_cache_folder . $ahmed_cache_prefix . md5( $_SERVER[ 'REQUEST_URI' ] );
+	
+	if ( file_exists( $f ) && !is_admin() )
+	{
+		ob_end_flush();
+		
+		echo file_get_contents( $f );
+		
+		die();
+	}
+}
+
+function ahmed_clear_cache()
+{
+	global $ahmed_cache_folder;
+	global $ahmed_cache_prefix;
+	
+	$h	= opendir( $ahmed_cache_folder );
+	
+	if ( $h )
+	{
+		while ( ( $f = readdir( $h ) ) !== false )
+		{
+			if ( strstr( $f, $ahmed_cache_prefix ) )
+			{
+				unlink( $ahmed_cache_folder . $f );
+			}
+		}
+		
+		closedir( $h );
+	}
+}
+
+function ahmed_save_cache()
+{
+	global $ahmed_cache_folder;
+	global $ahmed_cache_prefix;
+	
+	$f	= $ahmed_cache_folder . $ahmed_cache_prefix . md5( $_SERVER[ 'REQUEST_URI' ] );
+	$h 	= ob_get_contents();
+	
+	if ( !file_exists( $f ) )
+	{
+		file_put_contents( $f, $h );
+	}
+	
+	ob_end_flush();
+}
+
 function ahmed_loop($t='')
 {
 	if ( have_posts() ): ?>
@@ -35,3 +92,10 @@ function ahmed_loop($t='')
 		<?php get_search_form(); ?>
 	<?php endif;
 }
+
+add_action( 'clean_post_cache', 'ahmed_clear_cache' );
+add_action( 'delete_post', 'ahmed_clear_cache' );
+add_action( 'posts_selection', 'ahmed_check_cache' );
+add_action( 'save_post', 'ahmed_clear_cache' );
+add_action( 'shutdown', 'ahmed_save_cache', 0 );
+add_action( 'update_option', 'ahmed_clear_cache' );
