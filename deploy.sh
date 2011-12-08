608880df1f9e0cbdb9d1270d47d50f11a78f2d83
Ahmed Nuaman <ahmed@ahmednuaman.com>
2011-12-08 14:11:23 +0000

    updated deploy script to include wordpress

--- deploy.sh
+++ deploy.sh
@@ -18,4 +18,11 @@ rsync -e ssh --recursive --progress --verbose --compress \
 	--exclude 'dynamic.php' \
 	public/ ahmednua@fsmg.co.uk:~/www/
 
+echo 'Uploading theme...'
+rsync -e ssh --recursive --progress --verbose --compress \
+	--exclude '.svn/' \
+	--exclude '.DS_Store' \
+	--exclude 'style.css' \
+	private/wordpress/ ahmednua@fsmg.co.uk:~/www/blog/wp-content/themes/ahmednuaman/
+
 echo 'Done!'
