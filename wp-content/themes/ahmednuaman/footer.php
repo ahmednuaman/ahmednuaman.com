12694f4f2cde07b9af3bf37af71a16667d9c2d66
Ahmed Nuaman <ahmed@ahmednuaman.com>
2012-05-26 20:41:24 +0100

    setting up js

--- wp-content/themes/ahmednuaman/footer.php
+++ wp-content/themes/ahmednuaman/footer.php
@@ -2,6 +2,9 @@
 		<?php wp_footer(); ?>
 		<?php if ( WP_DEBUG ): ?>
 			<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+			<script src="<?php echo get_template_directory_uri(); ?>/assets/js/suitcase.js"></script>
+		<?php else; ?>
+			<script src="<?php echo get_template_directory_uri(); ?>/assets/js/packaged.js"></script>
 		<?php endif; ?>
 	</body>
 </html>
