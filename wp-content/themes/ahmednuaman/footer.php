d5bd6ee52bcd650f5ff308dfebe2b61e6324d81d
Ahmed Nuaman <ahmed@ahmednuaman.com>
2012-05-26 20:44:04 +0100

    sorted ; issue

--- wp-content/themes/ahmednuaman/footer.php
+++ wp-content/themes/ahmednuaman/footer.php
@@ -3,7 +3,7 @@
 		<?php if ( WP_DEBUG ): ?>
 			<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 			<script src="<?php echo get_template_directory_uri(); ?>/assets/js/suitcase.js"></script>
-		<?php else; ?>
+		<?php else: ?>
 			<script src="<?php echo get_template_directory_uri(); ?>/assets/js/packaged.js"></script>
 		<?php endif; ?>
 	</body>
