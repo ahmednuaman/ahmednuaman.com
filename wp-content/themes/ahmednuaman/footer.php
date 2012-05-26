da7783c869408b8566889454f1ebe2decf846969
Ahmed Nuaman <ahmed@ahmednuaman.com>
2012-05-26 20:27:22 +0100

    added lr

--- wp-content/themes/ahmednuaman/footer.php
+++ wp-content/themes/ahmednuaman/footer.php
@@ -1,4 +1,7 @@
 		</div>
 		<?php wp_footer(); ?>
+		<?php if ( WP_DEBUG ): ?>
+			<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+		<?php endif; ?>
 	</body>
 </html>
