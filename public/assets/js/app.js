e359525c86ac01c6b2bc8b7dd15f2b976d14af65
Ahmed Nuaman <ahmed@ahmednuaman.com>
2012-12-04 19:41:03 +0000

    added app js

---
+++ public/assets/js/app.js
@@ -0,0 +1,32 @@
+String.prototype.linkify = function()
+{
+    var str = this.toString();
+
+    str = str.replace(/(https?:\/\/[^\s]+)/gim, '<a href="$1">$1</a>', str);
+    str = str.replace(/([#|@][^\s]+)/gim, '<a href="http://twitter.com/$1">$1</a>', str);
+
+    return str;
+}
+
+$(function()
+{
+    function getTweets()
+    {
+        var tweets = [ ];
+
+        $.getJSON('https://api.twitter.com/1/statuses/user_timeline.json?screen_name=ahmednuaman&count=5&callback=?', function(data)
+        {
+            var ul = $('#twitter-feed').find('ul').empty();
+            var tweet;
+
+            for (var i = data.length - 1; i >= 0; i--)
+            {
+                tweet = data[i];
+
+                ul.append('<li>' + tweet.text.linkify() + '</li>');
+            }
+        });
+    }
+
+    getTweets();
+});
