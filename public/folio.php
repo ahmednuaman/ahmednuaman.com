bdf65669419a9c4b392ff067f579e7854a202bd6
Ahmed Nuaman <ahmed@ahmednuaman.com>
2012-12-05 14:12:04 +0000

    added bio

--- public/folio.php
+++ public/folio.php
@@ -22,11 +22,40 @@ $work = get_work_entries();
     </ul>
 </div>
 <div id="content" class="row">
-    <?php foreach ($work as $item): ?>
-        <a href="http://<?php echo $item->link; ?>" title="<?php echo $item->title; ?>" style="background-image: url(<?php echo $item->thumb; ?>);">
-            <span>
-                <?php echo $item->title; ?>
-            </span>
-        </a>
-    <?php endforeach; ?>
+    <div id="work" class="row">
+        <?php foreach ($work as $item): ?>
+            <a href="http://<?php echo $item->link; ?>" title="<?php echo $item->title; ?>" style="background-image: url(<?php echo $item->thumb; ?>);">
+                <span>
+                    <?php echo $item->title; ?>
+                </span>
+            </a>
+        <?php endforeach; ?>
+    </div>
+    <div id="bio" class="row">
+        <div class="span2">
+            <p>
+                I'm Ahmed, a freelance developer and designer. I graduated from Ravensbourne College in 2007 with a 1st class degree in Interaction Design. Since then I've worked for small and large agencies and clients including Google and YouTube, BBH, Unilever, Samsung, Johnson &amp; Johnson, TUI, BBC and may more.
+            </p>
+            <p>
+                My core development skills are in...
+            </p>
+            <ul>
+                <li>
+                    <strong>Backend:</strong>
+                    Python, PHP, and NodeJS
+                </li>
+                <li>
+                    <strong>Front end:</strong>
+                    HTML5 (Canvas, CSS3, etc...), JavaScript, and AS3
+                </li>
+                <li>
+                    <strong>Platforms and systems:</strong>
+                    Google App Engine, Heroku, Django, WordPress, CodeIgniter, Mocha TDD, Backbone, and PureMVC
+                </li>
+            </ul>
+            <p>
+                I'm a firm believer in writing code with DRY, MVC, and TDD principles in mind, even if it's a small single-page app or large content-driven site.
+            </p>
+        </div>
+    </div>
 </div>
